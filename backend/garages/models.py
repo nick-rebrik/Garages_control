5836f05caecd54681081a43a3664e9ef4fd6a8ac
nick-rebrik <77996995+nick-rebrik@users.noreply.github.com>
2022-02-11 21:28:53 +0100

    add models

--- backend/garages/models.py
+++ backend/garages/models.py
@@ -2,12 +2,52 @@ from django.db import models
 
 
 class Garage(models.Model):
-    pass
+    numebr = models.IntegerField('Номер гаражу')
+    renter = models.ForeignKey(
+        'Renter',
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        verbose_name='Орендар',
+        related_name='garage'
+    )
+    tariff = models.DecimalField('Тариф')
+
+    class Meta:
+        ordering = ('numebr',)
+        verbose_name = 'Гараж'
+        verbose_name_plural = 'Гаражи'
+
+    def __str__(self):
+        return f'{self.numebr}'
 
 
 class Renter(models.Model):
-    pass
+    name = models.CharField('Ім\'я Фамілія', max_length=250)
+    number = models.IntegerField('Номер телефону')
+    balance = models.DecimalField('Баланс')
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Орендар'
+        verbose_name_plural = 'Орендарі'
+
+    def __str__(self):
+        return f'{self.name}'
 
 
 class Indicators(models.Model):
-    pass
+    indicator = models.IntegerField('Показник лічильника')
+    garage = models.ForeignKey(
+        Garage,
+        on_delete=models.CASCADE,
+        verbose_name='Гараж',
+        related_name='indicators'
+    )
+    amount = models.DecimalField('Сумма до оплати', blank=True, null=True)
+    date = models.DateTimeField('Дата', auto_now_add=True)
+
+    class Meta:
+        ordering = ('-date',)
+        verbose_name = 'Показник лічильника'
+        verbose_name_plural = 'Показники лічильника'
