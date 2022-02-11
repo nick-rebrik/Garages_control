30a37fd18080315667ffbce1bba3a49e0e9b3970
nick-rebrik <77996995+nick-rebrik@users.noreply.github.com>
2022-02-11 23:11:51 +0100

    add pre_delete_indicators signal

--- backend/garages/signals.py
+++ backend/garages/signals.py
@@ -1,31 +1,39 @@
-from django.db.models.signals import pre_save
+from decimal import Decimal
+
+from django.db.models.signals import pre_delete, pre_save
 from django.dispatch import receiver
 
 from .models import Indicators
 
 
 def indicators_calculation(instance, indicator):
-    last_indicator = instance.garage.indicators.first
-    return (indicator - last_indicator) * instance.garage.tariff
+    try:
+        last_indicator = instance.garage.indicators.last().indicator
+    except AttributeError:
+        last_indicator = 0
+    return Decimal((indicator - last_indicator) * instance.garage.tariff)
 
 
 @receiver(pre_save, sender=Indicators)
-def pre_save_indicators(created, **kwargs):
-    instance = kwargs['instance']
+def pre_save_indicators(instance, **kwargs):
     indicator = instance.indicator
     renter = instance.garage.renter
-    if renter:
-        if created:
+    if renter and instance.garage.indicators.exists():
+        if instance.pk is None:
             instance.amount = indicators_calculation(instance, indicator)
-            instance.save()
             renter.balance -= instance.amount
             renter.save()
+        else:
+            renter.balance += instance.amount
+            renter.save()
+            instance.amount = indicators_calculation(instance, indicator)
+            renter.balance -= instance.amount
+            renter.save()
+
+
+@receiver(pre_delete, sender=Indicators)
+def pre_delete_indicators(instance, **kwargs):
+    renter = instance.garage.renter
+    if renter:
         renter.balance += instance.amount
-        renter.save()
-        previous_indicator = Indicators.objects.filter(
-            garage=instance.garage
-        )[-2]
-        instance.amount = indicators_calculation(instance, previous_indicator)
-        instance.save()
-        renter.balance -= instance.amount
         renter.save()
