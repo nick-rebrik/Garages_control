b9b5a1d3d14a2159f7850de7b98683a982349315
nick-rebrik <77996995+nick-rebrik@users.noreply.github.com>
2022-02-12 18:00:04 +0100

    add views

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from garages.models import Garage, Renter
+from rest_framework.generics import get_object_or_404
+
+from .serializers import (GarageSerializer, IndicatorsSerializer,
+                          RenterCreateAndUpdateSerializer,
+                          RenterReadSerializer)
+
+
+class GarageViewSet(viewsets.ModelViewSet):
+    queryset = Garage.objects.all().select_related('renter')
+    serializer_class = GarageSerializer
+
+
+class RenterViewSet(viewsets.ModelViewSet):
+    queryset = Renter.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return RenterReadSerializer
+        return RenterCreateAndUpdateSerializer
+
+
+class IndicatorsViewSet(viewsets.ModelViewSet):
+    serializer_class = IndicatorsSerializer
+
+    def get_queryset(self):
+        garage = get_object_or_404(Garage, id=self.kwargs['garage_id'])
+        return garage.indicators.all()
