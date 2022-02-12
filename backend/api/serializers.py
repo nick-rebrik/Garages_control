233deec921f3cf8f83076ec90e9c106990145278
nick-rebrik <77996995+nick-rebrik@users.noreply.github.com>
2022-02-12 18:00:16 +0100

    add serializers

---
+++ backend/api/serializers.py
@@ -0,0 +1,69 @@
+from rest_framework import serializers
+
+from garages.models import Garage, Renter, Indicators
+
+
+class RenterCreateAndUpdateSerializer(serializers.ModelSerializer):
+    balance = serializers.DecimalField(
+        max_digits=7,
+        decimal_places=2,
+        default='0.00'
+    )
+
+    class Meta:
+        model = Renter
+        fields = (
+            'name',
+            'number',
+            'balance',
+        )
+
+
+class RenterReadSerializer(RenterCreateAndUpdateSerializer):
+    class Meta:
+        model = Renter
+        fields = (
+            'name',
+            'number',
+            'balance',
+        )
+        read_only_fields = ('balance',)
+
+
+class GarageSerializer(serializers.ModelSerializer):
+    renter = serializers.SlugRelatedField(
+        queryset=Renter.objects.all(),
+        slug_field='name',
+        required=False,
+    )
+
+    class Meta:
+        model = Garage
+        fields = (
+            'number',
+            'renter',
+            'tariff',
+        )
+
+
+class IndicatorsSerializer(serializers.ModelSerializer):
+    garage = serializers.SlugRelatedField(
+        queryset=Garage.objects.all(),
+        slug_field='number',
+    )
+
+    class Meta:
+        model = Indicators
+        fields = (
+            'garage',
+            'indicator',
+            'amount',
+            'date',
+        )
+        read_only_fields = (
+            'amount',
+            'date',
+        )
+
+    def create(self, validated_data):
+        return Indicators.objects.create(**validated_data)
